ffebf96f9021e3dd9992f9ca374937a44daee5f9
xcapde <xcapde@hotmail.com>
2022-06-10 09:46:42 +0200

    kata RPG combat fins 2.2 ok fet a classe per Sergi

--- src/main/java/katas/CharacterS.java
+++ src/main/java/katas/CharacterS.java
@@ -1,5 +1,46 @@
 package katas;
 
 public class CharacterS {
+    private int maxHealth = 1000;
+    private int health = 1000;
+    private int level = 1;
+    private boolean alive = true;
+
+    public int getHealth(){
+        return this.health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void attacksOther(CharacterS victim, int damage){
+        if ( victim == this) return;
+        victim.damage(damage);
+    }
+
+    private void damage(int damage) {
+        if (this.health - damage <= 0){
+            this.alive = false;
+            this.health = 0;
+            return;
+        }
+        this.health -= damage;
+    }
+
+    public void healOther(CharacterS character, int heal){
+        if (character != this) return;
+        character.heal(heal);
+    }
+
+    private void heal(int heal){
+        if (!this.alive) return; // CLÀUSULA SALVAGUARDA
+        this.health += heal;
+        if (this.health > maxHealth) this.health = maxHealth;
+    }
 
 }
--- src/test/java/katas/CharacterSTest.java
+++ src/test/java/katas/CharacterSTest.java
@@ -1,7 +1,95 @@
 package katas;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterSTest {
+
+    @Test
+    void characterStartsWithCorrectAtributes(){
+        var fighter = new CharacterS();
+
+        assertEquals(1000, fighter.getHealth());
+        assertEquals(1, fighter.getLevel());
+        assertEquals(true, fighter.isAlive());
+    }
+
+    @Test
+    void characterAttackAnotherCharacterAndRestFromHealth(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+
+        attacker.attacksOther(victim,50);
+
+        assertEquals(950, victim.getHealth());
+    }
+
+    @Test
+    void whenHealthBelowTo0CharacterDiesAndHealthTurns0(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+
+        attacker.attacksOther(victim,1500); //VALOR LIMIT ÉS 1000
+
+        assertEquals(false, victim.isAlive());
+        assertEquals(0, victim.getHealth());
+    }
+
+    // TEST MODIFICAT MÉS TARD SEGONS LA NOVA LÒGICA DE LA KATA 2.2 (victim heal itself)
+    @Test
+    void characterCanHealItself(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+
+        attacker.attacksOther(victim,50);
+        victim.healOther(victim, 20);
+
+        assertEquals(970, victim.getHealth());
+    }
+
+    @Test
+    void deadCharacterCannotBeHealed(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+        attacker.attacksOther(victim,1500);
+
+        attacker.healOther(victim, 600);
+
+        assertEquals(0, victim.getHealth());
+        assertEquals(false, victim.isAlive());
+    }
+
+    // TEST MODIFICAT MÉS TARD SEGONS LA NOVA LÒGICA DE LA KATA 2.2 (victim heal itself)
+    @Test
+    void characterCannotRise1000Health(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+        attacker.attacksOther(victim,500);
+
+        victim.healOther(victim, 600);
+
+        assertEquals(1000, victim.getHealth());
+    }
+
+    @Test
+    void characterCannotDamageItself(){
+        var attacker = new CharacterS();
+
+        attacker.attacksOther(attacker,500);
+
+        assertEquals(1000, attacker.getHealth());
+    }
+
+    @Test
+    void characterCanOnlyHealItself(){
+        var attacker = new CharacterS();
+        var victim = new CharacterS();
+
+        attacker.attacksOther(victim, 500);
+        attacker.healOther(victim,50);
+
+        assertEquals(500, victim.getHealth());
+    }
 
 }
